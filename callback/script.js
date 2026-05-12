8cf351c4b2cc3e0afdbb18171a2fe5d8efde9a1b
Fa'iq Fadlurrohman <faiqfadlurrohman1002@gmail.com>
2026-05-12 09:00:19 +0700

    Menambahkan file index.html dan script.js ke dalam folder callback

---
+++ callback/script.js
@@ -0,0 +1,44 @@
+// callback
+
+// Synchronous Callback
+// function halo(nama) {
+//     alert(`Halo, ${nama}`);
+// }
+
+// function tampilkanPesan(callback) {
+//     const nama = prompt("Masukan Nama: ");
+//     callback(nama);
+// }
+
+// tampilkanPesan(halo);
+// tampilkanPesan(nama => alert(`Nama, ${nama}`));
+
+// const cus = [
+//     {
+//         "nama": "Beckham",
+//         "email": "beckham@beras3n.online",
+//         "alamat": "Awirarangan",
+//         "idAdmin": 1
+//     },
+//     {
+//         "nama": "Rizky",
+//         "email": "rizky@beras3n.online",
+//         "alamat": "Cidahu",
+//         "idAdmin": 1
+//     },
+//     {
+//         "nama": "Taufik",
+//         "email": "taufik@beras3n.online",
+//         "alamat": "Sindangagung",
+//         "idAdmin": 2
+//     }
+// ];
+
+// console.log("mulai");
+// cus.forEach(c =>{
+//     for (let i = 0; i < 10000000; i++) {
+//         let date = new Date();
+//     }
+//     console.log(c.nama);
+// });
+// console.log("selesai");
