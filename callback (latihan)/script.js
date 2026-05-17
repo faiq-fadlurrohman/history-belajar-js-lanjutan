f8106a277438ef53134b40b9fbc59c55326f3ae6
Fa'iq Fadlurrohman <faiqfadlurrohman1002@gmail.com>
2026-05-17 08:10:14 +0700

    Menambahkan error handling pada fitur pencarian (FF Movie Database)

--- callback (latihan)/script.js
+++ callback (latihan)/script.js
@@ -63,11 +63,42 @@
 
 const searchButton = document.querySelector(".search-button");
 searchButton.addEventListener("click", async function() {
-    const inputKeyword = document.querySelector(".input-keyword");
-    const movies = await getMovies(inputKeyword.value);
-    
-    updateUI(movies);
+    try {
+        const inputKeyword = document.querySelector(".input-keyword");
+        const movies = await getMovies(inputKeyword.value);
+        
+        updateUI(movies);
+    } catch(err) {
+        // console.log(err);
+        alert(err);
+    }
 });
+
+function getMovies(keyword) {
+    return fetch("http://www.omdbapi.com/?apikey=3628ca2c&s=" + keyword)
+        .then(response => {
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+
+                return response.json();
+            })
+        .then(response => {
+            if (response.Response === "False") {
+                throw new Error(response.Error);
+            }
+
+            return response.Search;
+        });
+}
+
+function updateUI(movies) {
+    let cards = "";
+    movies.forEach(m => cards += showCards(m));
+
+    const movieContainer = document.querySelector(".movie-container");
+    movieContainer.innerHTML = cards;
+}
 
 // event binding
 document.addEventListener("click", async function(e) {
@@ -89,20 +120,6 @@ function updateUIDetail(md) {
     const movieDetail = showMovieDetail(md);
     const modelBody = document.querySelector(".modal-body");
     modelBody.innerHTML = movieDetail;
-}
-
-function getMovies(keyword) {
-    return fetch("http://www.omdbapi.com/?apikey=3628ca2c&s=" + keyword)
-        .then(response => response.json())
-        .then(response => response.Search);
-}
-
-function updateUI(movies) {
-    let cards = "";
-    movies.forEach(m => cards += showCards(m));
-
-    const movieContainer = document.querySelector(".movie-container");
-    movieContainer.innerHTML = cards;
 }
 
 function showCards(m) {
